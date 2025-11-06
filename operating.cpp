0245031370a3a596913367571954d460704c8877
11249a218-arch <11249a218@kanchiuniv.ac.in>
2025-11-06 15:46:29 +0530

    Update operating.cpp

--- operating.cpp
+++ operating.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
 using namespace std;
-
 class Height {
 private:
     int feet;
     int inches;
-
 public:
     // Constructor
     Height(int f = 0, int i = 0) {
@@ -13,21 +11,15 @@ public:
         inches = i;
         normalize();
     }
-
-    // Function to normalize inches (e.g., 15 inches = 1 foot 3 inches)
     void normalize() {
         if (inches >= 12) {
             feet += inches / 12;
             inches = inches % 12;
         }
     }
-
-    // Display height
     void display() const {
         cout << feet << " feet " << inches << " inches";
     }
-
-    // Overloading '>' operator
     bool operator>(const Height& h) const {
         if (feet > h.feet)
             return true;
@@ -36,8 +28,6 @@ public:
         else
             return false;
     }
-
-    // Overloading '<' operator
     bool operator<(const Height& h) const {
         if (feet < h.feet)
             return true;
@@ -46,14 +36,10 @@ public:
         else
             return false;
     }
-
-    // Overloading '==' operator
     bool operator==(const Height& h) const {
         return (feet == h.feet && inches == h.inches);
     }
 };
-
-// Main function
 int main() {
     Height h1(5, 10);  // 5 feet 10 inches
     Height h2(6, 2);   // 6 feet 2 inches
@@ -61,11 +47,9 @@ int main() {
     cout << "Height 1: ";
     h1.display();
     cout << endl;
-
     cout << "Height 2: ";
     h2.display();
     cout << endl;
-
     // Compare heights
     if (h1 > h2)
         cout << "\nHeight 1 is greater than Height 2";
@@ -73,7 +57,7 @@ int main() {
         cout << "\nHeight 1 is smaller than Height 2";
     else if (h1 == h2)
         cout << "\nBoth heights are equal";
-
     cout << endl;
     return 0;
 }
+

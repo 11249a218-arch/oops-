2749ff18e09f4cadac2f0d4971dce4e58d8d457b
11249a218-arch <11249a218@kanchiuniv.ac.in>
2025-11-06 15:52:37 +0530

    Update devika2.cpp

--- devika2.cpp
+++ devika2.cpp
@@ -1,14 +1,11 @@
 #include <iostream>
 using namespace std;
-
 class Student {
 private:
     int rollNo;
     char name[50];
     float marks;
-
 public:
-    // Function to input student details
     void getData() {
         cout << "Enter Roll Number: ";
         cin >> rollNo;
@@ -17,35 +14,26 @@ public:
         cout << "Enter Marks: ";
         cin >> marks;
     }
-
-    // Function to display student details
     void displayData() {
         cout << "\nRoll Number: " << rollNo;
         cout << "\nName: " << name;
         cout << "\nMarks: " << marks << endl;
     }
 };
-
 int main() {
     int n;
     cout << "Enter number of students: ";
     cin >> n;
-
-    // Create array of Student objects
     Student students[100];  // assuming max 100 students
-
-    // Input data for n students
     for (int i = 0; i < n; i++) {
         cout << "\nEnter details for student " << i + 1 << ":\n";
         students[i].getData();
     }
-
-    // Display all student data
     cout << "\n--- Student Information ---\n";
     for (int i = 0; i < n; i++) {
         cout << "\nDetails of student " << i + 1 << ":";
         students[i].displayData();
     }
-
     return 0;
 }
+

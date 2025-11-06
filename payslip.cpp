b6ed456f4043456fd8d69b54fbea92e362a3d2c4
11249a218-arch <11249a218@kanchiuniv.ac.in>
2025-11-06 15:47:35 +0530

    Update payslip.cpp

--- payslip.cpp
+++ payslip.cpp
@@ -1,15 +1,12 @@
 #include <iostream>
 using namespace std;
-
 class Employee {
 private:
     int empID;
     char name[50];
     float basicSalary;
     float hra, da, pf, grossSalary, netSalary;
-
 public:
-    // Function to input employee details
     void getData() {
         cout << "Enter Employee ID: ";
         cin >> empID;
@@ -18,18 +15,13 @@ public:
         cout << "Enter Basic Salary: ";
         cin >> basicSalary;
     }
-
-    // Function to calculate salary components
     void calculateSalary() {
         hra = 0.20 * basicSalary;   // House Rent Allowance = 20% of basic
         da  = 0.10 * basicSalary;   // Dearness Allowance = 10% of basic
         pf  = 0.08 * basicSalary;   // Provident Fund = 8% of basic
-
         grossSalary = basicSalary + hra + da;
         netSalary = grossSalary - pf;
     }
-
-    // Function to display payslip
     void displayPayslip() {
         cout << "\n----------------------------------";
         cout << "\n           PAYSLIP";
@@ -49,13 +41,9 @@ public:
 
 int main() {
     Employee emp;
-
-    // Input and calculate salary
     emp.getData();
     emp.calculateSalary();
-
-    // Display payslip
     emp.displayPayslip();
-
     return 0;
 }
+

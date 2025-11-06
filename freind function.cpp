1be663c96ba5085f29729f3efc03b0d25f37e4d5
11249a218-arch <11249a218@kanchiuniv.ac.in>
2025-11-06 15:54:26 +0530

    Update freind function.cpp

--- freind function.cpp
+++ freind function.cpp
@@ -1,31 +1,21 @@
 #include <iostream>
 using namespace std;
-
 class Box {
 private:
     int length;
-
 public:
     // Constructor to initialize length
     Box(int l) {
         length = l;
     }
-
-    // Declare friend function
     friend void printLength(Box b);
 };
-
-// Definition of friend function
 void printLength(Box b) {
     // Can access private member 'length' directly
     cout << "Length of the box: " << b.length << endl;
 }
-
 int main() {
-    Box box1(15);
-
-    // Call friend function
+    Box box1(1
     printLength(box1);
-
     return 0;
 }

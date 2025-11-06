83c304851475cab96f70bc1ed3df551d1aec93bc
11249a218-arch <11249a218@kanchiuniv.ac.in>
2025-11-06 15:45:06 +0530

    Update object.cpp

--- object.cpp
+++ object.cpp
@@ -1,16 +1,49 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
-// Class definition
 class Book {
 private:
-    int id;
+    int bookID;
     string title;
     string author;
     float price;
-
 public:
-    // Function to input book details
-    void inputDetails() {
-        cout << "E
+    void getDetails() {
+        cout << "\nEnter Book ID: ";
+        cin >> bookID;
+        cin.ignore(); // clear input buffer
+        cout << "Enter Book Title: ";
+        getline(cin, title);
+        cout << "Enter Author Name: ";
+        getline(cin, author);
+        cout << "Enter Book Price: ";
+        cin >> price;
+    }
+    void showDetails() const {
+        cout << "\nBook ID: " << bookID;
+        cout << "\nTitle: " << title;
+        cout << "\nAuthor: " << author;
+        cout << "\nPrice: ₹" << price << endl;
+    }
+};
+int main() {
+    int n;
+    cout << "Enter number of books: ";
+    cin >> n;
+
+    // Declare an array of objects
+    Book books[100];  
+
+    // Input details for each book
+    for (int i = 0; i < n; i++) {
+        cout << "\nEnter details of book " << i + 1 << ":\n";
+        books[i].getDetails();
+    }
+    // Display details for all books
+    cout << "\n--- Library Book Details ---\n";
+    for (int i = 0; i < n; i++) {
+        cout << "\nBook " << i + 1 << " details:";
+        books[i].showDetails();
+    }
+    return 0;
+}

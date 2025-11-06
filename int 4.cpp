17bbbbd6ffcdf9ca72529298e2a17136e00d21a7
11249a218-arch <11249a218@kanchiuniv.ac.in>
2025-11-06 15:39:57 +0530

    Update int 4.cpp

--- int 4.cpp
+++ int 4.cpp
@@ -1,23 +1,16 @@
-#include <stdio.h>
-
-// Define an inline function to add two numbers
+#include <stdio.h
 inline int add(int a, int b) {
     return a + b;
 }
-
-// Another inline function for multiplication
 inline int multiply(int a, int b) {
     return a * b;
 }
-
 int main() {
     int x, y;
-
     printf("Enter two numbers: ");
     scanf("%d %d", &x, &y);
-
     printf("\nSum = %d", add(x, y));
     printf("\nProduct = %d\n", multiply(x, y));
-
     return 0;
 }
+

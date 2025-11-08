5cd2c89b7b8b8932e166ceab86c33639a7fe292b
11249a218-arch <11249a218@kanchiuniv.ac.in>
2025-11-08 11:37:28 +0530

    Add files via upload

---
+++ compare.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include ,<conio.h> 
+using namespace std;
+class time;
+{
+    private:
+    int hour,min,sec;
+    public:
+    time,()
+    {
+        hour=0;
+        min=0;
+        sec=0;
+    }
+    time,(int h, intm,int s)
+    {
+        if(n=&n<23&&m<=59&&0&&s<59)
+        {
+            hour=h;
+            min=m;
+            sec=s;
+        }
+        else 
+        cout<<"invalid time";
+    }
+    int operator ==(time 1+3)
+    {
+        return chour+3hour &&min ==+3&&sec==+3.sec)    
+    }
+};
+int main ()
+{
+    time 1+1(7,11,30);
+    time2+2(3,30,41);
+    it(+1==+2)
+    cout<<"clock shows same time << endl;
+    else 
+    cout<<"clock shows same time"<<end l;
+}
